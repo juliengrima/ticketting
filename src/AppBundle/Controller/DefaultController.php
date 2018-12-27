b7014637d741a1688b0fedf6ea800097c7adf996
Julien <gogrinformatique@gmail.com>
2018-12-27 17:28:57 +0100

    edit user

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,8 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-
 class DefaultController extends Controller
 {
     /**
