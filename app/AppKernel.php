e2af5123283966d4bc9adaee759d8ba9e2e802d4
Julien <gogrinformatique@gmail.com>
2018-12-27 16:24:47 +0100

    deletet calendar

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new SearchBundle\SearchBundle(),
-            new CalendarBundle\CalendarBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
